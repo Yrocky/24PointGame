d361ab9ed4006a62a3169b27ae0a99a8e450fa17
yrocky <983272765@qq.com>
2018-08-04 16:35:53 +0800

    Initial Commit

---
+++ 24PointGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  24PointGame
+//
+//  Created by user1 on 2018/8/4.
+//  Copyright © 2018年 Yrocky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 24PointGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  24PointGame
+//
+//  Created by user1 on 2018/8/4.
+//  Copyright © 2018年 Yrocky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
